bbe029f48803b55ab46e78e7b284093e66619ebe
alfi-s <alfisumadi@gmail.com>
2018-11-03 17:34:08 +0000

    Fixed 404 static route problem

--- server/routes.py
+++
@@ -1,6 +0,0 @@
-from server import app
-from flask import send_from_directory
-
-@app.route('/')
-def root():
-    return send_from_directory('../templates', 'index.html')
--- weather.py
+++ weather.py
@@ -1,1 +1,10 @@
-from server import app
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
