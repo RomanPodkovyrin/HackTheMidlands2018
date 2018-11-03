145b625e396922b14a479bc2d7f96163411d34ff
alfi-s <alfisumadi@gmail.com>
2018-11-03 23:57:03 +0000

    Minor tweaks to the backend code

--- src/python/generateData.py
+++ src/python/generateData.py
@@ -1,7 +1,7 @@
 import csv
 import random
 api_key = 'fddb1aa2d4034f6d105a0dd0defd9cd2'
-with open('synthetic_data.csv', mode='w') as datafile:
+datafile = open('synthetic_data.csv', mode='w') 
 
 data_writer = csv.writer(datafile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
@@ -11,8 +11,8 @@ tovalue = 1541286798    #11/03/2018 @ 11:13pm (UTC)
 
 for i in range (400):
     randomUnix = random.randint(fromvalue,tovalue)
-    temp =
-    humidity=
-    preci=
-    wind=
-    data_writer.writerow(['John Smith', 'Accounting', 'November'])
+    temp = random.randint(fromvalue, tovalue)
+    humidity= random.randint(fromvalue, tovalue)
+    preci= random.randint(fromvalue, tovalue)
+    wind= random.randint(fromvalue, tovalue)
+    data_writer.writerow([randomUnix, temp, humidity, preci, wind])
