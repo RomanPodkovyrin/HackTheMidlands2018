7af2e49259402c96d4a2f01ef7acb4cf43e68c06
alfi-s <alfisumadi@gmail.com>
2018-11-04 09:26:27 +0000

    Created a route to send location data to the server

--- weather.py
+++ weather.py
@@ -1,10 +1,17 @@
 from flask import Flask
 from flask import render_template
-app = Flask(__name__)
+from flask import request
+app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('/recommend')
+def recommend():
+    lon = request.args.get('lon')
+    lat = request.args.get('lat')
+    return ' Latitude: %s | Longitude: %s' % (lat, lon)
 
 if __name__ == '__main__':
     app.run(debug=True)
