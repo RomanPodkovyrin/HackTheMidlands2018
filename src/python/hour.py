fca0fcc9bd60803be0c695876950932980e111e4
Roman <RXP777@student.bham.ac.uk>
2018-11-04 11:53:26 +0000

    gob bless

--- src/python/hour.py
+++ src/python/hour.py
@@ -16,15 +16,15 @@ class Hour:
     def getString(self):
         feel = ""
         if self.layer == 0 :
-            feel = "Cold"
+            feel = "It's Cold. Dress warmly. Take 3 layers maybe?"
         elif self.layer == 1:
-            feel = "Freezing"
+            feel = "It's FREEZING. Dress extra warmly. 4 layers minimum."
         elif self.layer == 2:
-            feel = "Ok"
+            feel = "The weather's alright to be fair. You can dress with 2 layers."
         elif self.layer == 3:
-            feel = "Warm"
+            feel = "It's quite warm. Wear 1-2 layers at most."
         elif self.layer == 4:
-            feel = "Hot"
+            feel = "Oof ow it's hot. Just wear a t-shirt and shorts."
 
         #return    "\nTime: " + str(self.time) +"    Layer: " +str(feel) +  "\n    precipIntensity: "+  str(self.precipIntensity) + "\n    precipProbability: " + str(self.precipProbability) + "\n    Temperature: " + str(self.temperature) +"\n    Humidity: " + str(self.humidity) + "\n    WindSpeed: " + str(self.windSpeed)
-        return '\nTime:  '+ str(self.time) + '   Layer: ' +str(feel)
+        return '\nTime:  '+ str(self.time) + '   Recommendation: ' +str(feel)
